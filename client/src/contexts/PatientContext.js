3528274ebd71c084d425ac7ee17c6e5eefbaa00a
Ryan <ryanoconnor285@gmail.com>
2019-05-21 10:16:22 -0400

    set default settings

--- client/src/contexts/PatientContext.js
+++ client/src/contexts/PatientContext.js
@@ -2,13 +2,18 @@ import React, { useState } from 'react';
 
 export const PatientContext = React.createContext({});
 
-export const defaultSettings = {
-    currentPatient: {}
+export const defaultPatient = {
+        mrn: '1',
+        age: '87',
+        sex: 'M',
+        weight: '210',
+        height: "5'8",
+        bmi: '39.1',
 }
 
 export const PatientProvider = ({ children }) => {
-    const [patient, setPatient] = useState({})
-    return <PatientContext.Provider value={ [patient, setPatient] }>
+    const [currentPatient, setCurrentPatient] = useState(defaultPatient);
+    return <PatientContext.Provider value={ [currentPatient, setCurrentPatient] }>
             { children }
         </PatientContext.Provider>
 }
