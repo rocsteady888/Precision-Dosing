6c1d5a0ad2db926a33cb7244720076df3aaaed29
Ryan O'Connor <rocsteady888@gmail.com>
2019-05-29 20:05:40 -0400

    added medication data

--- client/src/contexts/PatientContext.js
+++ client/src/contexts/PatientContext.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 export const PatientContext = React.createContext({});
 
 export const defaultPatient = {
-        mrn: '1',
+        pid: '1',
         age: '87',
         sex: 'M',
         weight: '210',
@@ -11,10 +11,42 @@ export const defaultPatient = {
         bmi: '39.1',
         currentMedications: [
             {
-                genericName: '',
-                brandName: '',
-            }
-        ]
+                id: '1',
+                genericName: 'metoprolol',
+                brandName: 'Lopressor',
+                indication: 'hypertension',
+                precisionDosingAvailable: false,
+            },
+            {
+                id: '2',
+                genericName: 'rivaroxaban',
+                brandName: 'Xarelto',
+                indication: 'nonvalvular atrial fibrillation',
+                precisionDosingAvailable: true,
+            },
+            {
+                id: '3',
+                genericName: 'atorvastatin',
+                brandName: 'Lipitor',
+                indication: 'hyperlipidemia',
+                precisionDosingAvailable: false,
+            },
+            {
+                id: '4',
+                genericName: 'metformin',
+                brandName: 'Glucophage',
+                indication: 'type II diabetes',
+                precisionDosingAvailable: false,
+            },
+            {
+                id: '5',
+                genericName: 'cetirizine',
+                brandName: 'Zyrtec',
+                indication: 'treat hay fever and allergy symptoms, hives, and itching',
+                precisionDosingAvailable: false,
+            },
+        ],
+        lastUpdated: '05/19/2019 at 2:47am'
 }
 
 export const PatientProvider = ({ children }) => {
