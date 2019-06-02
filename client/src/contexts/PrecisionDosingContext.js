ffc668e621422331a070b5ba57c2010e5049c739
Ryan O'Connor <rocsteady888@gmail.com>
2019-06-02 14:20:40 -0400

    initial commit

---
+++ client/src/contexts/PrecisionDosingContext.js
@@ -0,0 +1,11 @@
+import React, { useState, createContext } from 'react'
+
+export const PrecisionDosingContext = createContext({});
+
+export const PrecisionDosingProvider = ({children}) => { 
+const [showDosing, setShowDosing] = useState(false)
+
+return <PrecisionDosingContext.Provider value={ [showDosing, setShowDosing] }>
+        {children}
+    </PrecisionDosingContext.Provider>
+}
