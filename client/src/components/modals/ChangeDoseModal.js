0412eb7815198353442c39dd45d460a3ebbc4c59
Ryan O'Connor <rocsteady888@gmail.com>
2019-06-30 23:38:09 -0400

    removed depracted syntax

--- client/src/components/modals/ChangeDoseModal.js
+++ client/src/components/modals/ChangeDoseModal.js
@@ -8,10 +8,10 @@ import Button from '@material-ui/core/Button';
 const styles = theme => ({
   paper: {
     position: 'absolute',
-    width: theme.spacing.unit * 50,
+    width: theme.spacing(50),
     backgroundColor: theme.palette.background.paper,
     boxShadow: theme.shadows[5],
-    padding: theme.spacing.unit * 4,
+    padding: theme.spacing(4),
     outline: 'none',
   },
 });
