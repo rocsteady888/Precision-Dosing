b08d4f9cebe2ed81295df4d413868b9a97562510
Ryan O'Connor <rocsteady888@gmail.com>
2019-06-30 23:36:51 -0400

    removed depracted syntax

--- client/src/components/DemoFactors.js
+++ client/src/components/DemoFactors.js
@@ -12,7 +12,7 @@ import Paper from '@material-ui/core/Paper';
 const styles = theme => ({
   root: {
     width: 650,
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: 'auto',
   },
   table: {
