4e296466fa7aac5035b2773537b9fafe90d03c9c
Ryan O'Connor <rocsteady888@gmail.com>
2019-06-30 23:37:24 -0400

    initial commit

---
+++ client/src/components/MedicationList.js
@@ -0,0 +1,40 @@
+import React, { useContext } from "react";
+
+import { MedicationContext } from "../contexts/MedicationContext";
+
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+
+const MedicationList = () => {
+  const [defaultMedications] = useContext(MedicationContext);
+  return (
+    <React.Fragment>
+      <List>
+        {defaultMedications.currentMedications.map(medication => {
+          return (
+            <div key={medication.id}>
+              <ListItem>
+                <ListItemText
+                  primary={medication.genericName}
+                  secondary={
+                    medication.brandName
+                      ? "Commonly known as: " + medication.brandName
+                      : null
+                  }
+                />
+                <ListItemText
+                  primary={medication.frequency}
+                />
+              </ListItem>
+              <Divider />
+            </div>
+          );
+        })}
+      </List>
+    </React.Fragment>
+  );
+};
+
+export default MedicationList;
