faf540ae84bd713d0f0f6d4e2e8079200506ba11
Ryan O'Connor <rocsteady888@gmail.com>
2019-07-07 12:13:15 -0400

    changed folder structure

--- client/src/DetailedView.js
+++ client/src/DetailedView.js
@@ -1,75 +1,30 @@
-import React, { useContext } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 
-import PatientDemographics from './components/PatientDemographics';
-import DrugMonitoringTabs from './components/DugMonitoringTabs';
+import Navbar from './components/navigation/Navbar';
+import PatientDemoForm from './components/web-view/PatientDemoForm';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
-import { PrecisionDosingContext } from './contexts/PrecisionDosingContext';
-
-import { withStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-
-const drawerWidth = 450;
-
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-  },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
-  drawerPaper: {
-    width: drawerWidth,
-  },
-  content: {
     flexGrow: 1,
-    padding: theme.spacing.unit * 3,
   },
-  toolbar: theme.mixins.toolbar,
-});
+  demoFormContainer: {
+    marginTop: 50
+  }
+}));
 
-function PermanentDrawer(props) {
-  const { classes } = props;
-  const [showDosing] = useContext(PrecisionDosingContext);
+export default function FullWidthGrid() {
+  const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            Precision Dosing
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-      >
-        <div className={classes.toolbar} />
-        <PatientDemographics />
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.toolbar} />
-          <DrugMonitoringTabs />
-      </main>
+    <Navbar />
+      <Grid container justify="center" alignItems="center" spacing={3} className={classes.demoFormContainer} >
+        <Grid item xs={12} sm={6}>
+          <PatientDemoForm />
+        </Grid>
+      </Grid>
     </div>
   );
 }
-
-PermanentDrawer.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(PermanentDrawer);
